3549117582898bb3fa3c23af6d89bd28718ef579
Ross-Sherlock <rasherlock@hotmail.co.uk>
2024-01-27 13:03:57 +0000

    Add initial backend logic and routes

---
+++ backend/src/User.ts
@@ -0,0 +1,14 @@
+export default interface User {
+    id: number;
+    avatar: string;
+    first_name: string;
+    last_name: string;
+    email: string;
+    emailVerified: boolean;
+    dob: string;
+    company: {
+      name: string;
+      department: string;
+    };
+    skills: string[];
+  }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,32 @@
 import express, { Request, Response } from "express";
+import User from "./User";
+import fs from "fs";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+let users: User[] = [];
+
+// Read mock user data
+try {
+  const data = fs.readFileSync("./src/users.json", "utf8");
+  users = JSON.parse(data) as User[];
+} catch (err) {
+  console.error(err);
+}
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Test");
+});
+
+app.get("/users", (req: Request, res: Response) => {
+  res.send(users);
+});
+
+app.get("/users/:id", (req: Request, res: Response) => {
+  const user_id = parseInt(req.params.id);
+  const user = users.find((u) => u.id === user_id);
+  user ? res.status(200).send(user) : res.status(404).send("User not found");
 });
 
 app.listen(port, () => {
